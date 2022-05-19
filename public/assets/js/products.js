46407f2d892c93b0259f2ecdeab9b1e4c6f788bf
FERRARIOloic <ferrarioloic@gmail.com>
2022-05-19 12:23:28 +0200

    json OK, création des cards

--- public/assets/js/products.js
+++ public/assets/js/products.js
@@ -9,117 +9,45 @@ vote_average
 
 const API_KEY = "7f1f1402996361135b864d977d5871b8";
 
-fetch(`../data/samsung.json`, {
+fetch(`public/assets/data/samsung.json`, {
         method: "GET"
     })
     .then(response => response.json())
     .then(data => {
 
-        // console.log(data); // <- {id: number, name: string}[]
+        console.log(data);
 
 
         data.results.forEach(element => {
-            let originalTitle = element.title;
-            let overview = element.overview;
-            let posterPath = element.poster_path;
-            let voteAverage = element.vote_average;
 
-            if (voteAverage >= 0 && voteAverage <= 1) {
-                voteStars = `
-                <div class="row">
-                    <div class="col-2"><img class="stars" src="public/assets/img/starHalf.svg"></div>
-                </div>`;
-            } else if (voteAverage > 1 && voteAverage <= 2) {
-                voteStars = `
-                <div class="row">
-                    <div class="col-2"><img class="stars" src="public/assets/img/starFull.svg"></div>
-                </div>`;
-            } else if (voteAverage > 2 && voteAverage <= 3) {
-                voteStars = `
-                <div class="row">
-                    <div class="col-2"><img class="stars" src="public/assets/img/starFull.svg"></div>
-                    <div class="col-2"><img class="stars" src="public/assets/img/starHalf.svg"></div>
-                </div>`;
-            } else if (voteAverage > 3 && voteAverage <= 4) {
-                voteStars = `
-                <div class="row">
-                    <div class="col-2"><img class="stars" src="public/assets/img/starFull.svg"></div>
-                    <div class="col-2"><img class="stars" src="public/assets/img/starFull.svg"></div>
-                </div>`;
-            } else if (voteAverage > 5 && voteAverage <= 6) {
-                voteStars = `
-                <div class="row">
-                    <div class="col-2"><img class="stars" src="public/assets/img/starFull.svg"></div>
-                    <div class="col-2"><img class="stars" src="public/assets/img/starFull.svg"></div>
-                    <div class="col-2"><img class="stars" src="public/assets/img/starHalf.svg"></div>
-                </div>`;
-            } else if (voteAverage > 6 && voteAverage <= 7) {
-                voteStars = `
-                <div class="row">
-                    <div class="col-2"><img class="stars" src="public/assets/img/starFull.svg"></div>
-                    <div class="col-2"><img class="stars" src="public/assets/img/starFull.svg"></div>
-                    <div class="col-2"><img class="stars" src="public/assets/img/starFull.svg"></div>
-                </div>`;
-            } else if (voteAverage > 7 && voteAverage <= 8) {
-                voteStars = `
-                <div class="row">
-                    <div class="col-2"><img class="stars" src="public/assets/img/starFull.svg"></div>
-                    <div class="col-2"><img class="stars" src="public/assets/img/starFull.svg"></div>
-                    <div class="col-2"><img class="stars" src="public/assets/img/starFull.svg"></div>
-                    <div class="col-2"><img class="stars" src="public/assets/img/starHalf.svg"></div>
-                </div>`;
-            } else if (voteAverage > 8 && voteAverage <= 9) {
-                voteStars = `
-                <div class="row">
-                    <div class="col-2"><img class="stars" src="public/assets/img/starFull.svg"></div>
-                    <div class="col-2"><img class="stars" src="public/assets/img/starFull.svg"></div>
-                    <div class="col-2"><img class="stars" src="public/assets/img/starFull.svg"></div>
-                    <div class="col-2"><img class="stars" src="public/assets/img/starFull.svg"></div>
-                </div>`;
-            } else if (voteAverage > 9 && voteAverage <= 10) {
-                voteStars = `
-                <div class="row">
-                    <div class="col-2"><img class="stars" src="public/assets/img/starFull.svg"></div>
-                    <div class="col-2"><img class="stars" src="public/assets/img/starFull.svg"></div>
-                    <div class="col-2"><img class="stars" src="public/assets/img/starFull.svg"></div>
-                    <div class="col-2"><img class="stars" src="public/assets/img/starFull.svg"></div>
-                    <div class="col-2"><img class="stars" src="public/assets/img/starHalf.svg"></div>
-                </div>`;
-            } else if (voteAverage == 10) {
-                voteStars = `
-                <div class="row">
-                    <div class="col-2"><img class="stars" src="public/assets/img/starFull.svg"></div>
-                    <div class="col-2"><img class="stars" src="public/assets/img/starFull.svg"></div>
-                    <div class="col-2"><img class="stars" src="public/assets/img/starFull.svg"></div>
-                    <div class="col-2"><img class="stars" src="public/assets/img/starFull.svg"></div>
-                    <div class="col-2"><img class="stars" src="public/assets/img/starFull.svg"></div>
-                </div>`;
-            }
+            let marque = element.marque;
+            let commercialName = element.nom_commercial;
+            let technicalName = element.nom_technique;
+            let tutorialName = element.nom_tutoriel;
+            console.log(commercialName);
 
             cardMovie.innerHTML += `
                         <div id="cardMovieCss" class="col-12 col-md-3">
                             <!-- CARD -->
                             <div class="row">
                                 <div id="imgMovie" class="col-4">
-                                <img src="https://image.tmdb.org/t/p/w500${posterPath}">
+                                <img style="width:50px;" src="https://trepidai-astuces.s3.amazonaws.com/images/modeles/${marque}_${tutorialName}.jpg">
                                 </div>
                                 <div id="infoMovie" class="col-8">
                                     <div id="titleMovie" class="row">
                                         <div class="col">
-                                            <strong>${originalTitle}</strong>
+                                            <strong>${commercialName}</strong>
                                         </div>
                                     </div>
                                     <div id="textMovie" class="row">
                                         <div class="col">
-                                        ${overview.substring(0, 90)}...
+                                        ${technicalName.substring(0, 90)}
                                         </div>
                                     </div>
                                     <div id="voteAverage" class="row">
                                         <div id="starsMovie" class="col-12">
                                         
 
-
-                                        ${voteStars}
 
                                         </div>
                                     </div>
@@ -129,21 +57,6 @@ fetch(`../data/samsung.json`, {
                     `;
         })
 
-        let img = document.querySelectorAll('#cardMovieCss');
-        let para = document.querySelector('#cardMovieCss');
-        // console.table(img);
-        
-        img.forEach(element => {
-            element.addEventListener('click', (event) => {
-                let imgValue = event.target.innerText;
-                console.log(imgValue);
-
-            })
-        })
 
 
     })
-
-    .catch(function (error) {
-        console.error(error);
-    });
