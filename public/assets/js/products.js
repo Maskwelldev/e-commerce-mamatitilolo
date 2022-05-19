f3a6c9bb30c29771c77eae20573e1747544531e7
FERRARIOloic <ferrarioloic@gmail.com>
2022-05-19 17:00:40 +0200

    modal OK

--- public/assets/js/products.js
+++ public/assets/js/products.js
@@ -21,6 +21,7 @@ fetch(`public/assets/data/samsung.json`, {
 
 
                     /*------------- RECUPERATION INFO JSON ET BOUTON POUR COMPARAISON ---------*/
+                    var productID = element.id;
                     let queryFamily = event.target.dataset.famille;
                     let familyData = element.famille;
 
@@ -28,7 +29,6 @@ fetch(`public/assets/data/samsung.json`, {
                     if (queryFamily == familyData) {
 
                         /*------------- SI OK, RECUPERATION DES DONNEES A AFFICHER ---------*/
-                        let productID = element.id;
                         let marque = element.marque;
                         let commercialName = element.nom_commercial;
                         let technicalName = element.nom_technique;
@@ -36,29 +36,74 @@ fetch(`public/assets/data/samsung.json`, {
 
                         /*------------- AFFICHAGE DES DONNEES ---------*/
                         productsVue.innerHTML += `
-                                <div id="cardMovieCss" class="col-12 col-md-3">
+                            <div class="owl-item active" style="width: 212px; margin-right: 20px;">
+                                <div class="single-product">
+                                    <div class="product-f-image">
+                                        <img src="https://trepidai-astuces.s3.amazonaws.com/images/modeles/${marque}_${tutorialName}.jpg">
+                                        <div class="product-hover">
+                                            <a href="#" class="add-to-cart-link"><i class="fa fa-shopping-cart"></i> Add to
+                                                cart</a>
+                                            <a href="single-product.html" class="view-details-link"><i class="fa fa-link"></i> See details</a>
+                                        </div>
+                                    </div>
+
+                                    <h2><a href="single-product.html">Sony microsoft</a></h2>
+
+                                    <div class="product-carousel-price">
+                                        <ins>$200.00</ins> <del>$225.00</del>
+                                    </div>
+                                </div>
+                            </div>
+
+                            
+                                <div class="col-12 col-md-3">
                                     <!-- CARD -->
-                                    <div class="row">
-                                        <div id="imgMovie" class="col-4">
+                                    <div class="row ">
+                                        <div class="col-4">
                                         <img style="width:50px;" src="https://trepidai-astuces.s3.amazonaws.com/images/modeles/${marque}_${tutorialName}.jpg">
                                         </div>
                                         <div id="infoMovie" class="col-8">
                                             <div id="titleMovie" class="row">
-                                                <div class="col">
+                                                <div class="col-12">
                                                 <i>${productID}</i> / <strong>${commercialName}</strong>
                                                 </div>
                                             </div>
                                             <div id="textMovie" class="row">
-                                                <div class="col">
+                                                <div class="col-10">
                                                 ${technicalName}
                                                 </div>
-                                            </div>
-                                            <div id="voteAverage" class="row">
-                                                <div id="starsMovie" class="col-12">
-                                                
-        
-        
+                                                <div class="col-2">
+                                                    <button id="btn" data-id="${productID}" type="button" class="btn" data-bs-toggle="modal"
+                                                        data-bs-target="#exampleModal${productID}">
+                                                        ?
+                                                    </button>
                                                 </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                
+
+                                
+                                    <div class="modal fade" id="exampleModal${productID}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                    <div class="modal-dialog">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title" id="staticBackdropLabel"><strong>Samsung ${element.nom_commercial}</strong> (${element.nom_technique})</h5>
+                                            </div>
+                                            <div class="modal-body">
+                                                <div class="col-6">
+                                                    <img style="width:100%;" src="https://trepidai-astuces.s3.amazonaws.com/images/modeles/${marque}_${tutorialName}.jpg">
+                                                </div>
+                                                <div class="col-6">
+                                                    <div class="row">
+                                                    <div class="col-12">
+                                                        Mémoire Ram : ${element.ram}
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Ajouter au panier</button>
                                             </div>
                                         </div>
                                     </div>
@@ -66,9 +111,55 @@ fetch(`public/assets/data/samsung.json`, {
                             `;
 
                     }
+
+
+
+                    // /*------------- FONCTION ONCLICK BOUTON CATEGORIES ---------*/
+                    // let infoProduct = document.querySelectorAll('#btn');
+                    // infoProduct.forEach(eachTableElement => {
+                    //     eachTableElement.onclick = (event) => {
+                    //         console.log(event.target.dataset.id);
+                    //         productIDModal = event.target.dataset.id;
+
+                    //         data.results.forEach(elementModal => {
+
+                    //             if (elementModal.id == productIDModal) {
+
+
+                    //                 productsVue.innerHTML += `
+                    //                 <div class="modal fade" id="exampleModal${productID}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    //                 <div class="modal-dialog">
+                    //                     <div class="modal-content">
+                    //                         <div class="modal-header">
+                    //                             <h5 class="modal-title" id="staticBackdropLabel"><strong>Samsung ${elementModal.nom_commercial}</strong> (${elementModal.nom_technique})</h5>
+                    //                         </div>
+                    //                         <div class="modal-body">
+                    //                             <p>La partie se joue à deux joueurs</p>
+                    //                             <p>Chaque joueur choisi entre <i>Papier</i>, <i>Feuille</i> ou <i>Ciseaux</i></p>
+                    //                             <p>Les points se comptent ainsi :
+                    //                                 <br>Pierre : perd face à Feuille, gagne face à Ciseaux
+                    //                                 <br>Feuille : perd face à Ciseaux, gagne face à Pierre
+                    //                                 <br>Ciseaux : perd face à Feuille, gagne face à Ciseaux
+                    //                             </p>
+                    //                             <p>Celui qui gagne marque un point </p>
+                    //                         </div>
+                    //                         <div class="modal-footer">
+                    //                             <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Jouer</button>
+                    //                         </div>
+                    //                     </div>
+                    //                 </div>
+                    //             </div>
+                    //             `;
+                    //             }
+
+
+                    //         });
+
+
+                    //     }
+                    // })
                 })
             }
-
         })
 
 
