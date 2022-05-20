9106df9844e0f80d48ba98d112cf5204dc29f58f
FERRARIOloic <ferrarioloic@gmail.com>
2022-05-20 13:34:44 +0200

    modal info modèle OK

--- public/assets/js/products.js
+++ public/assets/js/products.js
@@ -1,27 +1,21 @@
-/*------------- RECUPERATION DES DONNEES JSON ---------*/
-fetch(`public/assets/data/samsung.json`, {
-        method: "GET"
-    })
-    .then(response => response.json())
-    .then(data => {
+/*------------- FONCTION ONCLICK BOUTON CATEGORIES ---------*/
+let selectFamily = document.querySelectorAll('#categories');
+selectFamily.forEach(eachTableElement => {
+    eachTableElement.onclick = (event) => {
+        /*------------- RECUPERATION DES DONNEES JSON ---------*/
+        fetch(`public/assets/data/samsung.json`, {
+                method: "GET"
+            })
+            .then(response => response.json())
+            .then(data => {
 
-        console.log(data);
-
-
-        /*------------- FONCTION ONCLICK BOUTON CATEGORIES ---------*/
-        let selectFamily = document.querySelectorAll('button');
-        selectFamily.forEach(eachTableElement => {
-            eachTableElement.onclick = (event) => {
-
+                // console.log(data);
                 /*------------- NETTOYAGE DE L'AFFICHAGE ---------*/
                 productsVue.innerHTML = '';
-
                 /*------------- BOUCLAGE DES DONNEES DU JSON ---------*/
                 data.results.forEach(element => {
-
-
                     /*------------- RECUPERATION INFO JSON ET BOUTON POUR COMPARAISON ---------*/
-                    var productID = element.id;
+                    let productID = element.id;
                     let queryFamily = event.target.dataset.famille;
                     let familyData = element.famille;
 
@@ -54,8 +48,8 @@ fetch(`public/assets/data/samsung.json`, {
                                                 ${technicalName}
                                                 </div>
                                                 <div class="col-2">
-                                                    <button id="btn" data-id="${productID}" type="button" class="btn" data-bs-toggle="modal"
-                                                        data-bs-target="#exampleModal${productID}">
+                                                    <button data-id="${productID}" type="button" class="btn btnModalModel" data-bs-toggle="modal"
+                                                        data-bs-target="#modalModel">
                                                         ?
                                                     </button>
                                                 </div>
@@ -63,86 +57,63 @@ fetch(`public/assets/data/samsung.json`, {
                                         </div>
                                     </div>
                                 </div>
-                                
+                            `;
+                    }
+                })
 
-                                
-                                    <div class="modal fade" id="exampleModal${productID}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                    <div class="modal-dialog">
-                                        <div class="modal-content">
+            })
+
+            .finally(() => {
+                let verifBtn = document.querySelectorAll('.btnModalModel');
+                verifBtn.forEach((eachTableElement) => {
+                    // console.log(eachTableElement.dataset.id);
+
+                    eachTableElement.addEventListener('click', (event) => {
+                        console.log(event.target.dataset.id);
+
+                        /*------------- RECUPERATION DES DONNEES JSON ---------*/
+                        fetch(`public/assets/data/samsung.json`, {
+                                method: "GET"
+                            })
+                            .then(response => response.json())
+                            .then(data => {
+
+                                data.results.forEach(element => {
+
+                                    /*------------- COMPARAISON DES DONNEES ---------*/
+
+                                    let productID = element.id;
+                                    if (productID == event.target.dataset.id) {
+                                        console.log(element.marque);
+                                        console.log(element.nom_tutoriel);
+                                        infoModel.innerHTML = `
                                             <div class="modal-header">
-                                                <h5 class="modal-title" id="staticBackdropLabel"><strong>Samsung ${element.nom_commercial}</strong> (${element.nom_technique})</h5>
+                                            <h5 class="modal-title" id="staticBackdropLabel"><strong>Samsung ${element.nom_commercial}</strong>
+                                                (${element.nom_technique})</h5>
                                             </div>
                                             <div class="modal-body">
                                                 <div class="col-6">
-                                                    <img style="width:100%;" src="https://trepidai-astuces.s3.amazonaws.com/images/modeles/${marque}_${tutorialName}.jpg">
+                                                    <img style="width:100%;"
+                                                        src="https://trepidai-astuces.s3.amazonaws.com/images/modeles/${element.marque}_${element.nom_tutoriel}.jpg">
                                                 </div>
                                                 <div class="col-6">
-                                                    <div class="row">
-                                                    <div class="col-12">
+                                                    <p>
                                                         Mémoire Ram : ${element.ram}
-                                                    </div>
+                                                    </p>
                                                 </div>
-                                            </div>
-                                            <div class="modal-footer">
-                                                <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Ajouter au panier</button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            `;
+                                                <div class="modal-footer">
+                                                    <button id="btnLocalStorage" data-id="${productID}" type="button" class="btn btn-primary"
+                                                        data-bs-toggle="modal" data-bs-target="#modalCart${productID}">
+                                                        Ajouter au panier
+                                                    </button>
+                                                </div>
+                                            </div>`;
 
-                    }
-
-
-
-                    // /*------------- FONCTION ONCLICK BOUTON CATEGORIES ---------*/
-                    // let infoProduct = document.querySelectorAll('#btn');
-                    // infoProduct.forEach(eachTableElement => {
-                    //     eachTableElement.onclick = (event) => {
-                    //         console.log(event.target.dataset.id);
-                    //         productIDModal = event.target.dataset.id;
-
-                    //         data.results.forEach(elementModal => {
-
-                    //             if (elementModal.id == productIDModal) {
-
-
-                    //                 productsVue.innerHTML += `
-                    //                 <div class="modal fade" id="exampleModal${productID}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                    //                 <div class="modal-dialog">
-                    //                     <div class="modal-content">
-                    //                         <div class="modal-header">
-                    //                             <h5 class="modal-title" id="staticBackdropLabel"><strong>Samsung ${elementModal.nom_commercial}</strong> (${elementModal.nom_technique})</h5>
-                    //                         </div>
-                    //                         <div class="modal-body">
-                    //                             <p>La partie se joue à deux joueurs</p>
-                    //                             <p>Chaque joueur choisi entre <i>Papier</i>, <i>Feuille</i> ou <i>Ciseaux</i></p>
-                    //                             <p>Les points se comptent ainsi :
-                    //                                 <br>Pierre : perd face à Feuille, gagne face à Ciseaux
-                    //                                 <br>Feuille : perd face à Ciseaux, gagne face à Pierre
-                    //                                 <br>Ciseaux : perd face à Feuille, gagne face à Ciseaux
-                    //                             </p>
-                    //                             <p>Celui qui gagne marque un point </p>
-                    //                         </div>
-                    //                         <div class="modal-footer">
-                    //                             <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Jouer</button>
-                    //                         </div>
-                    //                     </div>
-                    //                 </div>
-                    //             </div>
-                    //             `;
-                    //             }
-
-
-                    //         });
-
-
-                    //     }
-                    // })
+                                    }
+                                })
+                            })
+                    });
                 })
-            }
-        })
-
-
-
-    })
+            })
+    }
+})
